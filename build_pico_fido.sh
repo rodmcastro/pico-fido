25c98230ba6243f98ec1d975d9185b2ed0ed862d
rodmcastro <rodmcastro@gmail.com>
2026-09-09 21:04:01 -0300

    Add Waveshare RP2350 Zero build target

--- build_pico_fido.sh
+++ build_pico_fido.sh
@@ -14,7 +14,7 @@ cd build_release
 
 PICO_SDK_PATH="${PICO_SDK_PATH:-../../pico-sdk}"
 SECURE_BOOT_PKEY="${SECURE_BOOT_PKEY:-../../ec_private_key.pem}"
-boards=("pico" "pico2")
+boards=("pico" "pico2" "waveshare_rp2350_zero")
 
 for board_name in "${boards[@]}"
 do
